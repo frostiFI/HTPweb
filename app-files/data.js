7f51f6ea021f5e275463df2d0cda9ee8ccec8865
frostiFI <74673829+frostiFI@users.noreply.github.com>
2021-10-30 11:55:17 +0300

    SorvanenLT v1
    Marzipano Tool

---
+++ app-files/data.js
@@ -0,0 +1,125 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sorvasen-lintutorni",
+      "name": "Sorvasen lintutorni",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.22747422272396278,
+        "pitch": -0.020924023382626444,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.065164730394347,
+          "pitch": -0.30691508265272205,
+          "rotation": 6.283185307179586,
+          "target": "1-sorvasen-lintutornissa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07010319608244586,
+          "pitch": 0.16288240760741957,
+          "title": "Sorvasen lintutorni",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.830125614808134,
+          "pitch": 0.16800575621143032,
+          "title": "Polkku&nbsp;",
+          "text": "Tornille lähtevän polun päässä on opastetaulu.&nbsp;<div><br><div>Käveltävää kertyy parisataa metriä vihreän kuusimetsän läpi.</div></div>"
+        },
+        {
+          "yaw": -2.504037505099925,
+          "pitch": 0.23062472589060334,
+          "title": "<ul><li><p style=\"margin-bottom: 0cm; line-height: 100%\">Hyvän\n\tTuulen paja</p>\n</li></ul>",
+          "text": "<p style=\"margin-bottom: 0cm; line-height: 100%\">Hyvän Tuulen paja on Uuraisten kunnan työpaja.&nbsp;</p><p style=\"margin-bottom: 0cm; line-height: 100%\"><br></p><p style=\"margin-bottom: 0cm; line-height: 100%\">https://uurainen.fi/</p>"
+        }
+      ]
+    },
+    {
+      "id": "1-sorvasen-lintutornissa",
+      "name": "Sorvasen lintutornissa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.13138495893040414,
+        "pitch": -0.06462420942172642,
+        "fov": 1.5541044068304541
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0978445256340876,
+          "pitch": 0.5687537478818712,
+          "rotation": 3.141592653589793,
+          "target": "0-sorvasen-lintutorni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.011030713471392062,
+          "pitch": 0.0055206581338982375,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">Sorvanen</span>",
+          "text": "<ul><li><p style=\"margin-bottom: 0cm; line-height: 100%\">Sorvanen on\n\tlähes umpeenkasvanut.&nbsp;</p></li><li><p style=\"margin-bottom: 0cm; line-height: 100%\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><br></span></p></li><li><p style=\"margin-bottom: 0cm; line-height: 100%\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">Sen&nbsp;</span><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">vedenpintaa on laskettu 1960-luvun alussa, minkä seurauksena järven\nkeskiosat ovat lähes kasvaneet umpeen. Järven pohjois- ja\neteläpäässä on pienialaiset avovesialueet.</span></p></li><li><p style=\"margin-bottom: 0cm; line-height: 100%\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><br></span></p></li><li><p style=\"margin-bottom: 0cm; line-height: 100%\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">Matalat ja ruohoiset\nrannat houkuttelevat alueelle puolisukeltajavesilintuja ja kahlaajia.</span></p></li><li><p style=\"margin-bottom: 0cm; line-height: 100%\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><br></span></p></li><li><p style=\"margin-bottom: 0cm; line-height: 100%\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">Kevättulvan aikaan järvi soveltuu vesilintujen lepäilypaikaksi.</span></p></li></ul>"
+        },
+        {
+          "yaw": 2.6849072908166445,
+          "pitch": 0.020036025035516047,
+          "title": "<p style=\"margin-bottom: 0cm; line-height: 100%\">&nbsp;Hyvän Tuulen paja</p>",
+          "text": "<p style=\"margin-bottom: 0cm; line-height: 100%\">Hyvän Tuulen paja on Uuraisten kunnan työpaja.</p><p style=\"margin-bottom: 0cm; line-height: 100%\"><br></p><p style=\"margin-bottom: 0cm; line-height: 100%\">https://uurainen.fi/</p>"
+        },
+        {
+          "yaw": 2.1570918293931394,
+          "pitch": 0.021336093562037917,
+          "title": "Pesa",
+          "text": "Pesimälajistoon ovat kuuluneet mustakurkku-uikku, laulujoutsen, valkoviklo ja kurki."
+        }
+      ]
+    }
+  ],
+  "name": "SorvasenLT",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
