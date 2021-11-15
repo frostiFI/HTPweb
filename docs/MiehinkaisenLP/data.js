e63da1ef7c28295ddc33f042a5acf380f08a901c
frostiFI <74673829+frostiFI@users.noreply.github.com>
2021-11-15 08:37:46 +0200

    w7 mon Miehinkäisen luontopolku
    Miehinkäisen luontopolku added

---
+++ docs/MiehinkaisenLP/data.js
@@ -0,0 +1,475 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-metsss",
+      "name": "Metsässä",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.35788844147201004,
+        "pitch": 0.02933422300544386,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.926278359964316,
+          "pitch": 0.2501222382889452,
+          "rotation": 9.42477796076938,
+          "target": "1-jklt"
+        },
+        {
+          "yaw": 2.0633179835640316,
+          "pitch": 0.15805906169674167,
+          "rotation": 0,
+          "target": "8-metsaukea"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.561702292826448,
+          "pitch": 0.16678945121828903,
+          "title": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; font-weight: normal; line-height: 108%;\">&nbsp;Mänty</p>",
+          "text": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; font-weight: normal; line-height: 108%;\">&nbsp;Hieno mäntymetsä.</p>"
+        }
+      ]
+    },
+    {
+      "id": "1-jklt",
+      "name": "Jäkälät",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4182527893333994,
+          "pitch": -0.16572017161808716,
+          "rotation": 0,
+          "target": "0-metsss"
+        },
+        {
+          "yaw": 2.01785398588296,
+          "pitch": 0.08155538835820053,
+          "rotation": 0,
+          "target": "2-pijnne"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.242171069728407,
+          "pitch": 0.10887034859736922,
+          "title": "Päijänne",
+          "text": "<p class=\"western\" style=\"margin-bottom: 0.28cm; line-height: 108%\">Järvi Miehinkäisen<span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">&nbsp;luontopolulla.&nbsp;</span></p>"
+        },
+        {
+          "yaw": -1.4254848705290062,
+          "pitch": 0.35886615150692336,
+          "title": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">\n<b>Jäkälät</b></p>",
+          "text": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">\n<b>Jäkälät ovat tässä.</b></p>"
+        }
+      ]
+    },
+    {
+      "id": "2-pijnne",
+      "name": "Päijänne",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.729646492116931,
+        "pitch": -0.028916706868173492,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5657618263449748,
+          "pitch": -0.20022304983661066,
+          "rotation": 0,
+          "target": "1-jklt"
+        },
+        {
+          "yaw": -0.5988919130344765,
+          "pitch": -0.05362835103220576,
+          "rotation": 0,
+          "target": "3-marjat"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.484757434208852,
+          "pitch": -0.3144685727558372,
+          "title": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Päijänne</p>",
+          "text": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Järvi Miehinkäisen luontopolulla on tässä.&nbsp;&nbsp;</p>"
+        }
+      ]
+    },
+    {
+      "id": "3-marjat",
+      "name": "Marjat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.304060520720883,
+          "pitch": 0.4552868400747965,
+          "rotation": 3.141592653589793,
+          "target": "4-ruska"
+        },
+        {
+          "yaw": 0.6257273132304562,
+          "pitch": -0.19647526191778297,
+          "rotation": 3.141592653589793,
+          "target": "2-pijnne"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8270136184124963,
+          "pitch": 0.3625639700725909,
+          "title": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">Puolukka</p>",
+          "text": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">Puolukka\non&nbsp;tässä.&nbsp;\n</p>"
+        },
+        {
+          "yaw": -1.3446219042957797,
+          "pitch": 0.09180246478117127,
+          "title": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Infotaulut</p>",
+          "text": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Vanha infotaulu on tässä.</p>"
+        }
+      ]
+    },
+    {
+      "id": "4-ruska",
+      "name": "Ruska",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6072086056683617,
+          "pitch": 0.03535257755808807,
+          "rotation": 3.141592653589793,
+          "target": "2-pijnne"
+        },
+        {
+          "yaw": 2.614347096356224,
+          "pitch": 0.05707201429143538,
+          "rotation": 0,
+          "target": "5-suo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.880681540965636,
+          "pitch": 0.5727426229785841,
+          "title": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Ruska</p>",
+          "text": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Ruska on tässä.</p>"
+        }
+      ]
+    },
+    {
+      "id": "5-suo",
+      "name": "Suo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.1357048696974914,
+        "pitch": -0.24873454311046928,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3159522627037674,
+          "pitch": -0.050039243649337095,
+          "rotation": 1.5707963267948966,
+          "target": "6-lintutorni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9663641507789027,
+          "pitch": 0.4824830974839607,
+          "title": "Polku",
+          "text": "<p class=\"western\" style=\"margin-bottom: 0.28cm; line-height: 108%\">Miehinkäisen lintutornille vievä polku on melko helppokulkuinen.\nReitti on merkitty maastoon sinisillä täplillä. Polku kulkee suo-\nja lampimaisemissa. Miehinkäisen rannalla maasto on märkää ja\nsaappaat<!-- Description of the nature trail to Miehinkäisen lintutorni -->\novat&nbsp;usein tarpeen.&nbsp;&nbsp;</p>"
+        },
+        {
+          "yaw": 0.8826580359134475,
+          "pitch": 0.09171490222321133,
+          "title": "Suo",
+          "text": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Suo on tässä.</p>"
+        }
+      ]
+    },
+    {
+      "id": "6-lintutorni",
+      "name": "Lintutorni",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.9767284683509203,
+        "pitch": -0.10648794528125194,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0113882427690974,
+          "pitch": -0.14923518042537154,
+          "rotation": 3.141592653589793,
+          "target": "5-suo"
+        },
+        {
+          "yaw": 0.9284256230171941,
+          "pitch": 0.13486984278595493,
+          "rotation": 6.283185307179586,
+          "target": "7-puro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.389865897271516,
+          "pitch": 0.16954840354342693,
+          "title": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Miehinkäinen</p>",
+          "text": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">Miehinkäinen on rehevä umpeen kasvava erämaalampi. Se on\nmerkittävä lintuvesi ja Natura 2000- kohde. Miehinkäinen\nhoukuttelee muutolla lepäileviä vesilintuja ja kahlaajia, mutta\nmyös muutamia merkittäviä pesimälajeja. Lammella on pesinyt\nlaulujoutsen, mustakurkku-uikku ja erilaisia sorsalajeja.</p>"
+        },
+        {
+          "yaw": -1.0372858723080665,
+          "pitch": 0.39408782763810635,
+          "title": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Miehinkäisen lintutorni&nbsp;&nbsp;</p>",
+          "text": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">Miehinkäisen lintutorni on maakunnankuulu lintutorni.&nbsp;&nbsp;</p>"
+        }
+      ]
+    },
+    {
+      "id": "7-puro",
+      "name": "Puro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7069237386556306,
+          "pitch": 0.11202405451539477,
+          "rotation": 0,
+          "target": "6-lintutorni"
+        },
+        {
+          "yaw": 2.988439384781083,
+          "pitch": 0.25143785192434365,
+          "rotation": 0,
+          "target": "8-metsaukea"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-metsaukea",
+      "name": "Metsäaukea",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0427928596056972,
+          "pitch": 0.029830483495445037,
+          "rotation": 0,
+          "target": "0-metsss"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7690336113358143,
+          "pitch": 0.16420425647438108,
+          "title": "<p class=\"western\" align=\"left\" style=\"margin-bottom: 0.28cm; line-height: 108%;\">&nbsp;Metsänhakkuu</p>",
+          "text": "Harvennushakkuu on kasvatushakkuuta, jossa metsästä poistuu puuston pääjoukon kasvua haittaavat ylitiheys ja ylispuut sekä merkityksettömiä aluspuita."
+        }
+      ]
+    }
+  ],
+  "name": "MiehinkäisenLP",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
