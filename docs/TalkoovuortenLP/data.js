a0fc49e3879d750aadeaedd8c3a39b00b60ff033
frostiFI <74673829+frostiFI@users.noreply.github.com>
2021-11-21 09:04:21 +0200

    w8 talkoovuori added

---
+++ docs/TalkoovuortenLP/data.js
@@ -0,0 +1,453 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ermaassa-talkoovuorella",
+      "name": "Erämaassa Talkoovuorella",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.127424294654478,
+          "pitch": 0.2142888661886193,
+          "rotation": 0,
+          "target": "2-kalliolla"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kivien-alla",
+      "name": "Kivien alla",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.8020846464118376,
+        "pitch": -0.02600832422937316,
+        "fov": 1.2335184273776312
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3979024872547381,
+          "pitch": -0.2884496738738527,
+          "rotation": 0,
+          "target": "5-men-huipulla"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kalliolla",
+      "name": "Kalliolla",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.86346166324123,
+        "pitch": -0.15175860261841123,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.241957494816586,
+          "pitch": -0.6183293716122833,
+          "rotation": 0,
+          "target": "3-huipulla-kivill"
+        },
+        {
+          "yaw": -1.0790119011568784,
+          "pitch": 0.5559112959270642,
+          "rotation": 3.141592653589793,
+          "target": "1-kivien-alla"
+        },
+        {
+          "yaw": 0.3565709791950731,
+          "pitch": 0.023656062178682546,
+          "rotation": 0,
+          "target": "0-ermaassa-talkoovuorella"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-huipulla-kivill",
+      "name": "Huipulla kivillä",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.134062384660952,
+        "pitch": 0.03236516108923482,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1460638023369345,
+          "pitch": 0.35403147662063894,
+          "rotation": 3.141592653589793,
+          "target": "2-kalliolla"
+        },
+        {
+          "yaw": -0.9507522805543616,
+          "pitch": 0.28023988058474103,
+          "rotation": 0,
+          "target": "4-infotaulu"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-infotaulu",
+      "name": "Infotaulu",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.9561572253568684,
+        "pitch": -0.05436248076373751,
+        "fov": 1.8339911980530281
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0966328364459894,
+          "pitch": 0.09025098841561885,
+          "rotation": 3.141592653589793,
+          "target": "6-portaat"
+        },
+        {
+          "yaw": -1.3286497243151238,
+          "pitch": 0.11994295395027876,
+          "rotation": 0,
+          "target": "5-men-huipulla"
+        },
+        {
+          "yaw": 1.7200184542318855,
+          "pitch": 0.13172149833587987,
+          "rotation": 0,
+          "target": "3-huipulla-kivill"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-men-huipulla",
+      "name": "Mäen huipulla",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.19115530369865397,
+        "pitch": -0.07077162588399588,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5760900543202254,
+          "pitch": 0.19329261790462127,
+          "rotation": 0,
+          "target": "4-infotaulu"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.045327155787314,
+          "pitch": 0.3900831762401431,
+          "title": "Jäkälät<br>",
+          "text": "Jäkäläpeitteiset kalliot kuluvat helposti ja liikkujien toivotaan pysyvän polulla ja noudattavan erityistä varovaisuutta.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "6-portaat",
+      "name": "Portaat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.7418369125141062,
+        "pitch": -0.11969909062034745,
+        "fov": 1.2335184273776312
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.26791896081036626,
+          "pitch": -0.38098871681269664,
+          "rotation": 0,
+          "target": "4-infotaulu"
+        },
+        {
+          "yaw": 2.890308189668925,
+          "pitch": 0.30478634669821325,
+          "rotation": 3.141592653589793,
+          "target": "7-polku-jatkuu"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-polku-jatkuu",
+      "name": "Polku jatkuu",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.7474982604927263,
+        "pitch": 0.3939792313152477,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10443127059576085,
+          "pitch": -0.06196681359346812,
+          "rotation": 0,
+          "target": "6-portaat"
+        },
+        {
+          "yaw": -3.0812614759839807,
+          "pitch": 0.09295467555710957,
+          "rotation": 3.141592653589793,
+          "target": "8-puro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9368372244331873,
+          "pitch": 0.9973698517473721,
+          "title": "Polku",
+          "text": "Talkoovuorten polku kulkee monipuolisessa metsämaisemassa ja polku on paikoin vaikeakulkuinen."
+        }
+      ]
+    },
+    {
+      "id": "8-puro",
+      "name": "Puro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.810091126033102,
+        "pitch": 0.1786044795046955,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.179465633641298,
+          "pitch": 0.3121247869215402,
+          "rotation": 6.283185307179586,
+          "target": "7-polku-jatkuu"
+        },
+        {
+          "yaw": -0.7939492261808887,
+          "pitch": 0.10008123406690927,
+          "rotation": 0,
+          "target": "9-sillan-vieress"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sillan-vieress",
+      "name": "Sillan vieressä",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.045595313996383,
+        "pitch": 0.2982284426022801,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4434284883722306,
+          "pitch": 0.45719099019018294,
+          "rotation": 3.141592653589793,
+          "target": "8-puro"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Talkoovuorten luontopolku",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
